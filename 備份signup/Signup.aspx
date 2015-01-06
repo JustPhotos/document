6918c3c16cf91f2c506c911f900dc3c4cdb32461
Huang Shih Hao <dt9450108@gmail.com>
2015-01-07 00:53:54 +0800

    備份Signup檔案

---
+++ 備份signup/Signup.aspx
@@ -0,0 +1,97 @@
+﻿<%@ Page Title="Just Photos - 註冊帳號" Language="VB" MasterPageFile="~/JustPhotoMaster.master" AutoEventWireup="false" CodeFile="Signup.aspx.vb" Inherits="Signup" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    <style type="text/css">
+        .auto-style1 {
+            width: 90px;
+        }
+        .auto-style2 {
+            height: 50px;
+        }
+
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <div id="signupBlock" class="signupStyle">
+        <table align="center">
+            <tr>
+                <th colspan="2">Sign up</th>
+            </tr>
+            <tr>
+                <td style="font-family: 微軟正黑體;" class="auto-style1 auto-style2">
+                    帳號
+                </td>
+                <td>
+                    <asp:TextBox ID="TextBoxAccount" runat="server" placeholder="數字與英文，至少六碼" MaxLength="15" class="signupNormalTextBox" AutoCompleteType="Disabled"></asp:TextBox>
+					<br />
+                    <asp:RequiredFieldValidator ID="accountRequired" runat="server" ErrorMessage="請輸入帳號" ControlToValidate="TextBoxAccount" Font-Bold="True" Font-Size="X-Small" ForeColor="Red" style="height: 13px; width: 188px" EnableClientScript="False" Display="Dynamic"></asp:RequiredFieldValidator>
+					<asp:CustomValidator ID="accountValidator" runat="server" ErrorMessage="至少輸入六碼" Font-Bold="True" Font-Size="X-Small" ForeColor="Red" style="height: 13px; width: 188px" ControlToValidate="TextBoxAccount" EnableClientScript="False" Display="Dynamic"></asp:CustomValidator>
+                </td>
+            </tr>
+            <tr>
+                <td style="font-family: 微軟正黑體" class="auto-style1 auto-style2">
+                    密碼
+                </td>
+                <td>
+                    <asp:TextBox ID="TextBoxPassword" runat="server" TextMode="Password" placeholder="輸入密碼" MaxLength="30" class="signupNormalTextBox" AutoCompleteType="Disabled"></asp:TextBox>
+					<br />
+                    <asp:RequiredFieldValidator ID="pwRequired" runat="server" ErrorMessage="請輸入密碼" ControlToValidate="TextBoxPassword" EnableClientScript="False" Font-Bold="True" Font-Size="X-Small" ForeColor="Red" style="height: 16px; width: 181px" Display="Dynamic"></asp:RequiredFieldValidator>
+                    <asp:CustomValidator ID="pwValidator" runat="server" ErrorMessage="至少六碼英數字元" ControlToValidate="TextBoxPassword" EnableClientScript="False" Font-Bold="True" Font-Size="X-Small" ForeColor="Red" style="height: 16px; width: 181px"></asp:CustomValidator>
+                </td>
+            </tr>
+			<tr>
+                <td style="font-family: 微軟正黑體" class="auto-style1 auto-style2">
+                    密碼確認
+                </td>
+                <td>
+                    <asp:TextBox ID="TextBoxPasswordConfirm" runat="server" TextMode="Password" placeholder="再輸入一次密碼" MaxLength="30" class="signupNormalTextBox" AutoCompleteType="Disabled"></asp:TextBox>
+					<br />
+                    <asp:RequiredFieldValidator ID="pwcRequired" runat="server" ErrorMessage="請輸入密碼確認" ControlToValidate="TextBoxPasswordConfirm" EnableClientScript="False" Font-Bold="True" Font-Size="X-Small" ForeColor="Red" style="height: 16px; width: 181px" Display="Dynamic"></asp:RequiredFieldValidator>
+					<asp:CompareValidator ID="pwcCompare" runat="server" ErrorMessage="密碼不同，請重新輸入" EnableClientScript="False" Font-Bold="True" Font-Size="X-Small" ForeColor="Red" style="height: 16px; width: 181px" ControlToCompare="TextBoxPassword" ControlToValidate="TextBoxPasswordConfirm"></asp:CompareValidator>
+                </td>
+            </tr>
+			<tr>
+                <td style="font-family: 微軟正黑體" class="auto-style1 auto-style2">
+                    Email
+                </td>
+                <td>
+                    <asp:TextBox ID="TextBoxEmail" runat="server" placeholder="輸入信箱" MaxLength="50" class="signupNormalTextBox" AutoCompleteType="Disabled"></asp:TextBox>
+					<br />
+                    <asp:RequiredFieldValidator ID="emailRequired" runat="server" ErrorMessage="請輸入信箱" ControlToValidate="TextBoxEmail" EnableClientScript="False" Font-Bold="True" Font-Size="X-Small" ForeColor="Red" style="height: 16px; width: 181px" Display="Dynamic"></asp:RequiredFieldValidator>
+					<asp:RegularExpressionValidator ID="emailValidator" runat="server" ErrorMessage="信箱格式錯誤" ControlToValidate="TextBoxEmail" ValidationExpression="\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*" EnableClientScript="False" Font-Bold="True" Font-Size="X-Small" ForeColor="Red" style="height: 16px; width: 181px" Display="Dynamic"></asp:RegularExpressionValidator>
+                </td>
+            </tr>
+			<tr>
+                <td style="font-family: 微軟正黑體" class="auto-style1 auto-style2">
+                    名稱
+                </td>
+                <td>
+                    <asp:TextBox ID="TextBoxName" runat="server" placeholder="輸入姓名" MaxLength="30" class="signupNormalTextBox" AutoCompleteType="Disabled"></asp:TextBox>
+					<br />
+                    <asp:RequiredFieldValidator ID="nameRequired" runat="server" ErrorMessage="請輸入姓名" ControlToValidate="TextBoxName" EnableClientScript="False" Font-Bold="True" Font-Size="X-Small" ForeColor="Red" style="height: 16px; width: 181px" Display="Dynamic"></asp:RequiredFieldValidator>
+                </td>
+            </tr>
+			<tr>
+                <td style="font-family: 微軟正黑體" class="auto-style1">
+                    描述
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                </td>
+                <td>
+                    <asp:TextBox ID="TextBoxDescription" runat="server" placeholder="輸入個人描述(150字以內)" Columns="50" Height="70px" Rows="3" TextMode="MultiLine" Width="650px" style="overflow: hidden; resize: none;" MaxLength="150" AutoCompleteType="Disabled"></asp:TextBox>
+					<br />
+                    <asp:CustomValidator ID="descriptionValidator" runat="server" ErrorMessage="描述不能超過150字" Font-Bold="True" Font-Size="X-Small" ForeColor="Red" style="height: 13px; width: 188px" ControlToValidate="TextBoxDescription" EnableClientScript="False" Display="Dynamic"></asp:CustomValidator>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2" style="text-align: right">
+                    <asp:Button ID="BtnSubmit" runat="server" Text="註冊" Font-Names="微軟正黑體" />
+                    <asp:Button ID="BtnCancel" runat="server" Text="取消" Font-Names="微軟正黑體" />
+                </td>
+            </tr>
+        </table>
+    </div>
+</asp:Content>
+
---
+++ 備份signup/Signup.aspx.vb
@@ -0,0 +1,147 @@
+﻿
+Partial Class Signup
+    Inherits System.Web.UI.Page
+    Protected Sub accountValidator_ServerValidate(source As Object, args As ServerValidateEventArgs) Handles accountValidator.ServerValidate
+        Dim accountText As String = TextBoxAccount.Text
+        Dim rexUrl As New Regex("[^a-z0-9]")
+
+        If accountText.Length < 6 And accountText.Length > 0 Then
+            accountValidator.ErrorMessage = "至少輸入六碼"
+            args.IsValid = False
+            Exit Sub
+        ElseIf rexUrl.IsMatch(TextBoxAccount.Text) Then
+            accountValidator.ErrorMessage = "帳號請輸入數字與英文"
+            args.IsValid = False
+            Exit Sub
+        End If
+    End Sub
+
+    Protected Sub pwValidator_ServerValidate(source As Object, args As ServerValidateEventArgs) Handles pwValidator.ServerValidate
+        Dim pw As String = TextBoxPassword.Text
+
+        If pw.Length < 6 And pw.Length > 0 Then
+            pwValidator.ErrorMessage = "至少六碼的密碼組合"
+            args.IsValid = False
+            Exit Sub
+        End If
+    End Sub
+
+    Protected Sub descriptionValidator_ServerValidate(source As Object, args As ServerValidateEventArgs) Handles descriptionValidator.ServerValidate
+        If TextBoxDescription.Text.Length > 150 Then
+            args.IsValid = False
+            Exit Sub
+        End If
+    End Sub
+
+    Protected Sub BtnSubmit_Click(sender As Object, e As EventArgs) Handles BtnSubmit.Click
+        'get each information from textbox
+        Dim account As String = TextBoxAccount.Text
+        Dim password As String = TextBoxPassword.Text
+        Dim passwordConfirm As String = TextBoxPasswordConfirm.Text
+        Dim email As String = TextBoxEmail.Text
+        Dim name As String = TextBoxName.Text
+        Dim description As String = TextBoxDescription.Text
+        Dim headPictureFileNmae As String = ""
+
+        Dim checkValidator As Boolean = accountRequired.IsValid And accountValidator.IsValid And pwValidator.IsValid And pwRequired.IsValid And pwcRequired.IsValid And pwcCompare.IsValid And _
+                emailRequired.IsValid And emailValidator.IsValid And nameRequired.IsValid And descriptionValidator.IsValid
+        If Not checkValidator Then
+            Exit Sub
+        End If
+
+        'process upload head picture
+        'Dim path As String = Server.MapPath("~\")
+        'Dim fileOK As Boolean = False
+
+        'If FileUploadHeadPic.HasFile Then
+        '    Dim allowedExtensions As String() = {".jpg", ".jpeg", ".png", ".gif"}
+        '    Dim fileExtension As String = System.IO.Path.GetExtension(FileUploadHeadPic.FileName).ToLower()
+
+        '    For i As Integer = 0 To allowedExtensions.Length - 1
+        '        If fileExtension = allowedExtensions(i) Then
+        '            fileOK = True
+        '        End If
+        '    Next
+        '    If fileOK Then
+        '        Try
+        '            'rename file
+        '            headPictureFileNmae = System.IO.Path.GetFileNameWithoutExtension(FileUploadHeadPic.FileName)
+        '            headPictureFileNmae = account & "_jptDB_hdp" & fileExtension
+
+        '            FileUploadHeadPic.PostedFile.SaveAs(path & headPictureFileNmae)
+        '            ImageHeadPicture.ImageUrl = headPictureFileNmae
+        '            'Response.Write("<Script language='JavaScript'>alert('檔案上傳成功');</Script>")
+        '        Catch ex As Exception
+        '            Response.Write("<Script language='JavaScript'>alert('檔案上傳失敗" & ex.Message & "');</Script>")
+        '            Exit Sub
+        '        End Try
+        '    Else
+        '        Response.Write("<Script language='JavaScript'>alert('不支援圖片格式以外之檔案上傳');</Script>")
+        '        Exit Sub
+        '    End If
+        'End If
+
+        'save sign up information to database
+        Dim conn1 As System.Data.SqlClient.SqlConnection
+        conn1 = New System.Data.SqlClient.SqlConnection
+        conn1.ConnectionString = ConfigurationManager.ConnectionStrings("JustPhotoDBConnStr").ConnectionString.ToString()
+        Dim cmd1 As System.Data.SqlClient.SqlCommand
+        cmd1 = New System.Data.SqlClient.SqlCommand
+        cmd1.Connection = conn1
+        cmd1.CommandType = Data.CommandType.StoredProcedure
+        cmd1.CommandText = "CREATEACCOUNT"
+
+        '@ID int,
+        '@account nvarchar(15),
+        '@password nvarchar(30),
+        '@name nvarchar(30),
+        '@email nvarchar(50),
+        '@description nvarchar(150),
+        '@headPicture nvarchar(25),
+
+        cmd1.Parameters.Add(New System.Data.SqlClient.SqlParameter("@ID", System.Data.SqlDbType.Int))
+        cmd1.Parameters.Add(New System.Data.SqlClient.SqlParameter("@account", System.Data.SqlDbType.NVarChar, 15))
+        cmd1.Parameters.Add(New System.Data.SqlClient.SqlParameter("@password", System.Data.SqlDbType.NVarChar, 30))
+        cmd1.Parameters.Add(New System.Data.SqlClient.SqlParameter("@name", System.Data.SqlDbType.NVarChar, 30))
+        cmd1.Parameters.Add(New System.Data.SqlClient.SqlParameter("@email", System.Data.SqlDbType.NVarChar, 50))
+        cmd1.Parameters.Add(New System.Data.SqlClient.SqlParameter("@description", System.Data.SqlDbType.NVarChar, 150))
+        cmd1.Parameters.Add(New System.Data.SqlClient.SqlParameter("@headPicture", System.Data.SqlDbType.NVarChar, 25))
+
+        cmd1.Parameters("@ID").Value = System.DBNull.Value
+        cmd1.Parameters("@account").Value = account
+        cmd1.Parameters("@password").Value = password
+        cmd1.Parameters("@name").Value = name
+        cmd1.Parameters("@email").Value = email
+        cmd1.Parameters("@description").Value = description
+        cmd1.Parameters("@headPicture").Value = headPictureFileNmae
+
+        conn1.Open()
+        Dim dr1 As System.Data.SqlClient.SqlDataReader
+        dr1 = cmd1.ExecuteReader
+        If dr1.Read Then
+            Dim getRetCode As Integer = Integer.Parse(dr1(0).ToString)
+
+            Select Case getRetCode
+                Case 0
+                    Response.Write("<Script language='JavaScript'>alert('註冊成功！');</Script>")
+                    Response.Redirect("~/Default.aspx")
+                Case 1
+                    Response.Write("<Script language='JavaScript'>alert('帳號已經存在，請修改！');</Script>")
+                Case 2
+                    Response.Write("<Script language='JavaScript'>alert('信箱已經存在，請修改！');</Script>")
+                Case 9
+                    Response.Write("<Script language='JavaScript'>alert('資料已經更新！');</Script>")
+                Case Else
+                    Response.Write("<Script language='JavaScript'>alert('哪邊出錯了？！');</Script>")
+            End Select
+
+
+        End If
+        dr1.Close()
+        conn1.Close()
+    End Sub
+
+    Protected Sub BtnCancel_Click(sender As Object, e As EventArgs) Handles BtnCancel.Click
+        Response.Redirect("~\Default.aspx")
+    End Sub
+End Class
